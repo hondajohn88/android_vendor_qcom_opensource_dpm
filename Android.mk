f998b38d20209e71195fcdab201c9a34e08ed72f
hondajohn88 <hondajohn88@gmail.com>
2017-01-07 12:13:37 -0800

    Commit for Frankenstein AOSP 7.1.1.

--- Android.mk
+++ Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
 LOCAL_NO_STANDARD_LIBRARIES := true
 LOCAL_MODULE:= tcmiface
-LOCAL_JAVA_LIBRARIES := core-oj core-libart
+LOCAL_JAVA_LIBRARIES := core-libart
 LOCAL_SRC_FILES := $(call all-subdir-java-files)
 include $(BUILD_STATIC_JAVA_LIBRARY)
 
